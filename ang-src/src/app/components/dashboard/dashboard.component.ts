dbaf28d911f1f9d4c18cd246d1ec394d77028848
vedha gnanam <vetha.gnanam98@gmail.com>
2018-08-26 23:08:06 +0530

    added quill

--- ang-src/src/app/components/dashboard/dashboard.component.ts
+++ ang-src/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { QuillEditorComponent } from 'ngx-quill/src/quill-editor.component';
+// add image resize module
+import ImageResize from 'quill-image-resize-module';
+import Quill from 'quill';
 
 @Component({
   selector: 'app-dashboard',
